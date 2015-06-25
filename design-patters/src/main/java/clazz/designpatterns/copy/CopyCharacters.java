3c5f5265746dd09c20c83420e3ef7196bda52085
Leandro Cruz <leandro.saad@gmail.com>
2015-06-25 13:31:33 -0300

    Using plain/old Stdin

--- design-patters/src/main/java/clazz/designpatterns/copy/CopyCharacters.java
+++ design-patters/src/main/java/clazz/designpatterns/copy/CopyCharacters.java
@@ -1,24 +1,26 @@
 package clazz.designpatterns.copy;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class CopyCharacters
 {
+	private static final int SIZE = 100;
+	
 	public static void main(String[] args)
 		throws Exception
 	{
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String input;
-		while((input = reader.readLine()) != null)
+		byte[] buffer = new byte[SIZE];
+		int count;
+		while((count = System.in.read(buffer)) != -1)
 		{
-			if("q".equals(input))
+			String input = new String(buffer);
+			if(input.trim().equals("q"))
 			{
 				System.out.println("Bye!");
 				return;
 			}
-			System.out.println(input);
+			System.out.println(input.trim());
+			Arrays.fill(buffer, (byte) 0);
 		}
 	}
-
 }
--- design-patters/src/main/java/clazz/designpatterns/copy/CopyCharactersToStderr.java
+++ design-patters/src/main/java/clazz/designpatterns/copy/CopyCharactersToStderr.java
@@ -1,20 +1,22 @@
 package clazz.designpatterns.copy;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class CopyCharactersToStderr
 {
+	private static final int SIZE = 100;
+
 	private static final boolean useStderr = true;
 
 	public static void main(String[] args)
 		throws Exception
 	{
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String input;
-		while((input = reader.readLine()) != null)
+		byte[] buffer = new byte[SIZE];
+		int count;
+		while((count = System.in.read(buffer)) != -1)
 		{
-			if("q".equals(input))
+			String input = new String(buffer);
+			if(input.trim().equals("q"))
 			{
 				if(useStderr)
 				{
@@ -24,17 +26,19 @@ public class CopyCharactersToStderr
 				{
 					System.out.println("Bye!");
 				}
-				
 				return;
 			}
 			if(useStderr)
 			{
-				System.err.println(input);
+				System.err.println(input.trim());
 			}
 			else
 			{
-				System.out.println(input);
+				System.out.println(input.trim());
 			}
+
+			Arrays.fill(buffer, (byte) 0);
 		}
+
 	}
 }

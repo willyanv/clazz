cf2611895b312e1c3e5ea533115340e92ec6985c
Leandro Cruz <leandro.saad@gmail.com>
2015-06-25 13:06:27 -0300

    Code skeleton

---
+++ design-patters/src/main/java/up/designpatterns/CopyCharacters.java
@@ -0,0 +1,24 @@
+package up.designpatterns;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CopyCharacters
+{
+	public static void main(String[] args)
+		throws Exception
+	{
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String input;
+		while((input = reader.readLine()) != null)
+		{
+			if("q".equals(input))
+			{
+				System.out.println("Bye!");
+				return;
+			}
+			System.out.println(input);
+		}
+	}
+
+}
